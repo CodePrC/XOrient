a6dd5ecae208c3ce8ba111c31e2712b4315ad9e7
PrC <prashant.x.chouhan@gsk.com>
2020-11-18 14:41:16 +0000

    wip

---
+++ resume/client/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Controller } from '@angular/common';
+import { OnInit } from '@angular/core';
+
+@Controller({
+    
+})
+
+export class DashboardController implements OnInit{
+    
+    constructor(){
+
+    }
+
+    ngOnInit(){
+
+    }
+}
